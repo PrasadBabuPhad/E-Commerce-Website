4899b1ba4b3e52b1ceafa40c06cc5c308c745f4f
PrasadBabuPhad <pphad4925@gmail.com>
2026-01-31 22:47:03 +0530

    Update README with project description

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ const userRoutes = require('./routes/user');
 const productRoutes = require('./routes/product');
 const orderRoutes = require('./routes/order');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const app = express(); 
+const PORT = process.env.PORT || 5000; 
 
 app.use(cors());
 app.use(express.json());
